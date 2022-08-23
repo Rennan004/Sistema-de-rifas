a8fa0dff6dafa0824f53b22f4a0dd48c7c607e49
Rennan004 <rennan.guilherme63@gmail.com>
2022-08-23 09:38:07 -0300

    a

--- src/vendedor/model/login-vendedor.php
+++ src/vendedor/model/login-vendedor.php
@@ -1,27 +1,35 @@
-<?php 
+<?php
 
-    include('../../conexao/conn.php');
-    $SENHA = $_REQUEST['SENHA'];
-    $LOGIN = $_REQUEST['LOGIN'];
-    $sql = $pdo->query("SELECT *, count(ID) as achou FROM VENDEDOR WHERE LOGIN = '".$_REQUEST['LOGIN']."' AND SENHA = '".md5($_REQUEST['SENHA'])."'");
+session_start(); //Inicio a sessão para coletar das as informações existentes na sessão do navegador
 
-    while($resultado = $sql->fetch(PDO::FETCH_ASSOC)){
-        if($resultado['achou'] == 1){
-            session_start();
-            $_SESSION['NOME'] = $resultado['NOME'];
-            $_SESSION['TIPO'] = $resultado['TIPO_ID'];
-            $dados = array{
-                'tipo' => 'success',
-                'mensagem' => 'Login correto.'
-                'TYPE' => $resultado['TIPO_ID'],
+session_destroy();
+// Obter a nossa conexão com o banco de dados
+include('../../conexao/conn.php');
+$SENHA = $_REQUEST['SENHA'];
+$LOGIN = $_REQUEST['LOGIN'];
+// Gerar uma consulta em banco de dados com as informações digitadas no formulário de login
+$sql = $pdo->query("SELECT ID, NOME, COUNT(ID) AS achou, TIPO_ID FROM VENDEDOR WHERE LOGIN = '$LOGIN' AND SENHA = '$SENHA' 
+");
+
+// Gero as devidas comparações
+while ($resultado = $sql->fetch(PDO::FETCH_ASSOC)) {
+        if($resultado['achou'] >= 1){
+        session_start(); // Inicio uma sessão --> é um local onde eu guardo informações para serem utilizadas posteriormente
+        $_SESSION['ID'] = $resultado['ID'];
+        $_SESSION['NOME'] = $resultado['NOME'];
+        $_SESSION['TIPO'] = $resultado['TIPO_ID'];
+        $dados = array(
+            'tipo' => 'success',
+            'mensagem' => 'Login correto!',
+            'TYPE' => $resultado['TIPO_ID'],
             'ID' => $resultado['ID']
-            };
-        }else{
-            $dados = array{
-                'tipo' => 'error',
-                'mensagem' => 'Login e/ou senha incorreto.'
-            };
-        }
+        );
+    }else{
+        $dados = array(
+            'tipo' => 'error',
+            'mensagem' => 'Nome de usuário ou senha INCORRETOS.'
+        );
     }
+}
 
-    echo json_encode($dados);
+echo json_encode($dados);
--- src/vendedor/model/validate-vendedor.php
+++ src/vendedor/model/validate-vendedor.php
@@ -1,16 +1,21 @@
 <?php
 
-    session_start();
+    session_start(); //Inicio a sessão PHP
 
-    if(!isset($_SESSION['NOME']) && !isset($_SESSION['TIPO'])){
+    // Verifico se existe sessão aberta com as seguintes informações --- ID e NOME
+    if(!isset($_SESSION['ID']) && !isset($_SESSION['NOME'])){
         $dados = array(
             'tipo' => 'error',
-            'mensagem' => 'Você não está autenticado no sistema.'
+            'mensagem' => 'Você não está autenticado para utilizar o sistema, por favor realize o login.'
         );
     }else{
         $dados = array(
             'tipo' => 'success',
-            'mensagem' => 'Seja bem vindo, '.$_SESSION['NOME']
+            'mensagem' => utf8_encode($_SESSION['NOME']),
+            'tipo_usuario' => $_SESSION['TIPO'],
+            'ID' =>  $_SESSION['ID'],
+            'TIPO_USUARIO' => $_SESSION['TIPO']
+
         );
     }
 
